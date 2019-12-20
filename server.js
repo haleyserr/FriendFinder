eb06b1b5b8ff2e53986fdbafe2f7716d78a6202b
Haley Serr <haleykpelletier@gmail.com>
2019-12-19 20:32:06 -0600

    Changing Landing Link in server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 //Home
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "./app/public/home.html"));
   });
 
 
